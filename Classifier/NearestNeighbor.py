7786b229a038753cebbaa4515c540a19013480e4
Mohammad <mghmgh1281375@gmail.com>
2018-12-03 15:48:32 +0330

    Adding K functionality

--- Classifier/NearestNeighbor.py
+++ Classifier/NearestNeighbor.py
@@ -7,15 +7,16 @@ import numpy as np
 class NearestNeighbor(Classifier):
     def __init__(self, k=1, *args, **kwargs):
         super(NearestNeighbor, self).__init__(*args, **kwargs)
+        self.k = k
     
     def predict(self, X):
         # TODO: Incomplete
         ret = []
         for x in X:
-            ret = self.y[np.argmin(Distance.euclidean_distance(x, self.X), axis=0)]
+            ret = self.y[np.argsort(Distance.euclidean_distance(x, self.X), axis=0)[:self.k]]
         return np.array(ret)
 
 
 if __name__ == "__main__":
-    pred = NearestNeighbor(k=1).fit([[1, 2], [4, 2]], [1, 2]).predict([[2, 2]])
+    pred = NearestNeighbor(k=2).fit([[1, 2], [4, 2]], [1, 2]).predict([[2, 2]])
     print(pred)
